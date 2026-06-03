28949f7bdae23d6807f90f875b71cdb95cfca3eb
Daniel Heward-Mills <5824561+danielhmills@users.noreply.github.com>
2026-06-03 15:48:14 -0400

    Add quick setup SQL for Databricks Bakehouse

---
+++ bakehouse/quick_setup.sql
@@ -0,0 +1,55 @@
+
+-- Attach Databricks Tables for Bakehouse
+
+ATTACH TABLE "bakehouse"."sales_customers" 
+PRIMARY KEY(customerID)
+AS "databricks"."bakehouse"."sales_customers"
+FROM 'databricks_odbc';
+
+vdd_disconnect_data_source('databricks_odbc');
+
+GRANT SELECT ON "databricks"."bakehouse"."sales_customers" TO SPARQL_SELECT;
+
+ATTACH TABLE "bakehouse"."media_customer_reviews" 
+PRIMARY KEY(new_id)
+AS "databricks"."bakehouse"."media_customer_reviews"
+FROM 'databricks_odbc';
+
+vdd_disconnect_data_source('databricks_odbc');
+
+GRANT SELECT ON "databricks"."bakehouse"."media_customer_reviews" TO SPARQL_SELECT;
+
+ATTACH TABLE "bakehouse"."sales_franchises" 
+PRIMARY KEY(franchiseID)
+AS "databricks"."bakehouse"."sales_franchises"
+FROM 'databricks_odbc';
+
+vdd_disconnect_data_source('databricks_odbc');
+
+GRANT SELECT ON "databricks"."bakehouse"."sales_franchises" TO SPARQL_SELECT;
+
+ATTACH TABLE "bakehouse"."sales_suppliers" 
+PRIMARY KEY(supplierID)
+AS "databricks"."bakehouse"."sales_suppliers"
+FROM 'databricks_odbc';
+
+vdd_disconnect_data_source('databricks_odbc');
+
+GRANT SELECT ON "databricks"."bakehouse"."sales_suppliers" TO SPARQL_SELECT;
+
+ATTACH TABLE "bakehouse"."sales_transactions" 
+PRIMARY KEY(transactionID)
+AS "databricks"."bakehouse"."sales_transactions"
+FROM 'databricks_odbc';
+
+vdd_disconnect_data_source('databricks_odbc');
+
+GRANT SELECT ON "databricks"."bakehouse"."sales_transactions" TO SPARQL_SELECT;
+
+SPARQL LOAD <https://raw.githubusercontent.com/danielhmills/databricks-sample-kg/refs/heads/main/bakehouse/r2rml.ttl> INTO <urn:databricks:bakehouse:r2rml>;
+
+EXEC ('SPARQL ' ||
+ DB.DBA.R2RML_MAKE_QM_FROM_G(
+            'urn:databricks:bakehouse:r2rml')
+);
+
