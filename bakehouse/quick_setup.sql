3af04f0d1ea697490dff6c8dc5daea7d25616f54
Daniel Heward-Mills <5824561+danielhmills@users.noreply.github.com>
2026-06-08 12:42:31 -0400

    Add Linked Data rewrite rules to quick_setup.sql
    - Add URL rewrite rules for entity URI dereferencing
    - Enable content negotiation for HTML (browsers) and RDF formats (clients)
    - Support navigation through Knowledge Graph via dereferenceable URIs

--- bakehouse/quick_setup.sql
+++ bakehouse/quick_setup.sql
@@ -54,3 +54,111 @@ EXEC ('SPARQL ' ||
             'urn:databricks:bakehouse:r2rml')
 );
 
+-- Virtual directories for instance data
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+'databricks_bakehouse_rule2',
+1,
+'(/databricks/bakehouse/[^#]*)',
+vector('path'),
+1,
+'/sparql?query=DESCRIBE+%%3Chttp%%3A//^{URIQADefaultHost}^%U%%23this%%3E+FROM+%%3Chttp%%3A//^{URIQADefaultHost}^/databricks-bakehouse-r2rml%%23%%3E&format=%U',
+vector('path', '*accept*'),
+null,
+'(text/rdf.n3)|(application/rdf.xml)|(text/n3)|(application/json)|(text/turtle)',
+2,
+null
+);
+
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+'databricks_bakehouse_rule4',
+1,
+'/databricks/bakehouse/stat([^#]*)',
+vector('path'),
+1,
+'/sparql?query=DESCRIBE+%%3Chttp%%3A//^{URIQADefaultHost}^/databricks/bakehouse/stat%%23%%3E+%%3Fo+FROM+%%3Chttp%%3A//^{URIQADefaultHost}^/databricks-bakehouse-r2rml%%23%%3E+WHERE+{+%%3Chttp%%3A//^{URIQADefaultHost}^/databricks/bakehouse/stat%%23%%3E+%%3Fp+%%3Fo+}&format=%U',
+vector('*accept*'),
+null,
+'(text/rdf.n3)|(application/rdf.xml)|(text/n3)|(application/json)|(text/turtle)',
+2,
+null
+);
+
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+'databricks_bakehouse_rule6',
+1,
+'/databricks/bakehouse/objects/([^#]*)',
+vector('path'),
+1,
+'/sparql?query=DESCRIBE+%%3Chttp%%3A//^{URIQADefaultHost}^/databricks/bakehouse/objects/%U%%3E+FROM+%%3Chttp%%3A//^{URIQADefaultHost}^/databricks-bakehouse-r2rml%%23%%3E&format=%U',
+vector('path', '*accept*'),
+null,
+'(text/rdf.n3)|(application/rdf.xml)|(text/n3)|(application/json)|(text/turtle)',
+2,
+null
+);
+
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+'databricks_bakehouse_rule1',
+1,
+'(/databricks/bakehouse/[^#]*)',
+vector('path'),
+1,
+'/describe/?url=http%%3A//^{URIQADefaultHost}^%U%%23this&graph=http%%3A//^{URIQADefaultHost}^/databricks-bakehouse-r2rml%%23&distinct=0',
+vector('path'),
+null,
+null,
+2,
+303
+);
+
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+'databricks_bakehouse_rule7',
+1,
+'/databricks/bakehouse/stat([^#]*)',
+vector('path'),
+1,
+'/describe/?url=http%%3A//^{URIQADefaultHost}^/databricks/bakehouse/stat%%23&graph=http%%3A//^{URIQADefaultHost}^/databricks-bakehouse-r2rml%%23',
+vector('path'),
+null,
+null,
+2,
+303
+);
+
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+'databricks_bakehouse_rule5',
+1,
+'/databricks/bakehouse/objects/(.*)',
+vector('path'),
+1,
+'/services/rdf/object.binary?path=%%2Fdatabricks%%2Fbakehouse%%2Fobjects%%2F%U&accept=%U',
+vector('path', '*accept*'),
+null,
+null,
+2,
+null
+);
+
+DB.DBA.URLREWRITE_CREATE_RULELIST ( 
+'databricks_bakehouse_rule_list1', 
+1, 
+vector ( 
+'databricks_bakehouse_rule1', 
+'databricks_bakehouse_rule7', 
+'databricks_bakehouse_rule5', 
+'databricks_bakehouse_rule2', 
+'databricks_bakehouse_rule4', 
+'databricks_bakehouse_rule6'
+)
+);
+
+DB.DBA.VHOST_REMOVE (lpath=>'/databricks/bakehouse');
+
+DB.DBA.VHOST_DEFINE (
+lpath=>'/databricks/bakehouse', 
+ppath=>'/', 
+vsp_user=>'dba', 
+is_dav=>0,
+is_brws=>0, 
+opts=>vector ('url_rewrite', 'databricks_bakehouse_rule_list1')
+);
